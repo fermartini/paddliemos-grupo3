9a5eab712c6107878d3b4a978041bc3a407e55e2
LeilaGiselleDeLuca <lgdl88@gmail.com>
2025-05-30 19:43:46 -0300

    Cambio de Home y agregado de Bruno

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Login from '../components/login/login';
-import Register from '../components/login/register'; 
+import Login from './components/Login';
+import Register from './components/Register'; 
+import Home from './components/Home'; 
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 
@@ -10,60 +11,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} /> 
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  );
-}
-
-function HomePage() {
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    navigate('/login');
-  };
-
-  const handleRegisterClick = () => {
-    navigate('/register');
-  };
-
-  return (
-    <main className='min-h-screen bg-base-200 flex items-center justify-center'>
-      <div className="absolute top-4 right-4 space-x-2">
-        <button className='btn btn-sm btn-primary mt-4' onClick={handleLoginClick}>
-          Ingresar
-        </button>
-        <button className='btn btn-sm btn-secondary mt-4' onClick={handleRegisterClick}>
-          Registrate
-        </button>
-      </div>
-      <section className='text-center space-y-8'>
-        <header>
-          <h1 className='text-5xl font-bold text-primary'>
-            Reservá tu turno en Paddliemos
-          </h1>
-          <p className='text-lg text-secondary'>
-            ¡Reserva fácilmente tu cancha de paddle para jugar con amigos o practicar!
-          </p>
-        </header>
-
-        <article className='mt-8'>
-          <h2 className='text-3xl font-semibold text-primary'>¿Cómo funciona?</h2>
-        </article>
-
-        <aside className='mt-8'>
-          <section className='card bg-base-100 shadow-xl w-96 mx-auto'>
-            <div className='card-body'>
-              <ul>
-                <li>1. Elige una fecha disponible.</li>
-                <li>2. Selecciona un horario para tu turno.</li>
-                <li>3. Confirma tu reserva y ¡listo!</li>
-              </ul>
-            </div>
-          </section>
-        </aside>
-      </section>
-    </main>
   );
 }
 
